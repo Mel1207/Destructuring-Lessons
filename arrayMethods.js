a4027c7ddcc86096efb08ccb77f16d67a2fac9f3
Mel <meljohnpualon@gmail.com>
2021-08-06 08:14:54 +0800

    added handson array methods

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,15 +1,19 @@
 'use strict'
+const laptopList = document.querySelector('.laptopList');
+const btnAddBrand = document.querySelector('.btn-submit');
+const inputBrand = document.querySelector('.brand-input');
 
-const laptopModels = [
+let laptopModels = [
     'Asus',
     'Lenovo',
     'Razer',
     'MSI',
     'Acer',
-    function specsCreator(coreProcessor, ram, rom, brandName) {
-        return `Thank you for purchasing ${brandName} ${coreProcessor}, ${ram}GB RAM, ${rom}TB of internal storage, happy gaming!`
-    } 
 ]
+
+const specsCreator = (coreProcessor, ram, rom, brandName) => {
+    return `Thank you for purchasing ${brandName} ${coreProcessor}, ${ram}GB RAM, ${rom}TB of internal storage, happy gaming!`
+} 
 
 const laptopProcessors = [
     'i3',
@@ -22,6 +26,8 @@ const laptopProcessors = [
     'r9'
 ]
 
+let brandNames = [];
+
 const numbers = [
     100,
     200,
@@ -31,3 +37,40 @@ const numbers = [
     600,
     700
 ]
+
+// laptopModels.forEach(laptop => {
+//     const createLi = document.createElement('li');
+//     createLi.textContent = laptop
+//     laptopList.appendChild(createLi).className = 'listed'
+// })
+
+
+laptopModels.forEach(brand => {
+    const createLi = document.createElement('li');
+    createLi.textContent = brand
+    laptopList.appendChild(createLi).className = 'listed'
+})
+
+btnAddBrand.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    
+
+    if(inputBrand.value === '') {
+        alert('please add brand')
+    } else {
+        const branNew = inputBrand.value;
+        laptopModels.push(branNew)
+
+        // console.log(branNew)
+        console.log(laptopModels)
+
+        
+    }
+
+    inputBrand.value = ''
+    
+})
+
+
+
